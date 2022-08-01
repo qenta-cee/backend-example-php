744288eaec1772d986f6dd7aed0bb0e06226a5b9
Alex Kacijan <alex.kacijan@qenta.com>
2022-08-01 07:39:03 +0200

    update deafult and environmental configuration

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -5,20 +5,20 @@
  * https://guides.qenta.com/prerequisites
  * - License can be found under:
  * https://github.com/qenta-cee/qcp-backend-example-php/blob/master/LICENSE.
- * 
+ *
  * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
  * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
  * PARTICULAR PURPOSE.
- * 
- * Please read the integration documentation before modifying this file!             
+ *
+ * Please read the integration documentation before modifying this file!
  */
 
   // customer id
   // e.g. D200001 for demonstration purposes only
   // for production mode, please use your personal customer id obtained by Qenta
   $customerId = getenv('QCP_BACKEND_CUSTOMER_ID') ?: "D200001";
-  $shopId = getenv('QCP_BACKEND_SHOP_ID') ?: 'alex';
+  $shopId = getenv('QCP_BACKEND_SHOP_ID') ?: '';
 
   // password for accessing Toolkit light
   $toolkitPassword = getenv('QCP_BACKEND_CUSTOMER_PASSWORD') ?: "jcv45z";
@@ -32,11 +32,9 @@
   // shortcut for selected language for all texts returned by a command
   $language = "en";
 
-	//--------------------------------------------------------------------------------//
+    //--------------------------------------------------------------------------------//
 
   // URLs for accessing  QPay Checkout Toolkit light
   $URL_QENTA_CHECKOUT_TOOLKIT_LIGHT = getenv('QCP_BACKEND_ENDPOINT') ?: "https://api.qenta.com/page/toolkit.php";
 
-	//--------------------------------------------------------------------------------//
-
-?>
+    //--------------------------------------------------------------------------------//
